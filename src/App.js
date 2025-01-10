8fa361cbfdb1856b5fa3d4f055732d834e9d7740
washington nieto <washington_nieto@hotmail.com>
2025-01-09 22:59:11 -0500

    implementacion con clases

--- src/App.js
+++ src/App.js
@@ -1,48 +1,98 @@
+import React from 'react';
 import './App.css';
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador'
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
-import { useState } from 'react';
 
-function App() {
+class App extends React.Component{
+  constructor(){
+    // cuando se define el constructor, tenemos que llamar a super()
+    // porque super() nos va permitir heredar todas la funcionalidad de React.Component
+    super();
+    this.state = {
+      numClics: 0
+    };
 
-  const [numClics, setNumClics] = useState(0);
-
-  const manejarClic = () =>{
-    // console.log('Clic');
-    setNumClics(numClics + 1);
+    this.manejarClic = this.manejarClic.bind(this);
+    this.reiniciarContador = this.reiniciarContador.bind(this);
   }
 
-  const reiniciarContador = () => {
-    // console.log('Reiniciar');
-    setNumClics(0);
-
+  manejarClic(){
+    this.setState(({ numclics }) => ({ numclics: numclics + 1 }));
   }
 
-  return (
-    <div className="App">
-      <div className='freecodecamp-logo-contenedor'>
-        <img 
-          className='freecodecamp-logo'
-          src={freeCodeCampLogo}
-          alt='Logo de freeCodeCamp'
-        />
+  reiniciarContador(){
+    this.setState({ numClics: 0 });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div className='freecodecamp-logo-contenedor'>
+          <img 
+            className='freecodecamp-logo'
+            src={freeCodeCampLogo}
+            alt='Logo de freeCodeCamp'
+          />
+        </div>
+        <div className='contenedor-principal'>
+          <Contador numClics = {this.state.numClics} />
+          <Boton 
+            texto='Click'
+            esBotonDeClic={true}
+            manejarClic ={this.manejarClic}
+          />
+          <Boton
+            texto='Reiniciar'
+            esBotonDeClic={false}
+            manejarClic ={this.reiniciarContador}        
+          />
+        </div>
       </div>
-      <div className='contenedor-principal'>
-        <Contador numClics = {numClics} />
-        <Boton 
-          texto='Click'
-          esBotonDeClic={true}
-          manejarClic ={manejarClic}
-        />
-        <Boton
-          texto='Reiniciar'
-          esBotonDeClic={false}
-          manejarClic ={reiniciarContador}        
-        />
-      </div>
-    </div>
-  );
+    );
+  }
 }
+
+
+// function App() {
+
+//   const [numClics, setNumClics] = useState(0);
+
+//   const manejarClic = () =>{
+//     // console.log('Clic');
+//     setNumClics(numClics + 1);
+//   }
+
+//   const reiniciarContador = () => {
+//     // console.log('Reiniciar');
+//     setNumClics(0);
+
+//   }
+
+//   return (
+//     <div className="App">
+//       <div className='freecodecamp-logo-contenedor'>
+//         <img 
+//           className='freecodecamp-logo'
+//           src={freeCodeCampLogo}
+//           alt='Logo de freeCodeCamp'
+//         />
+//       </div>
+//       <div className='contenedor-principal'>
+//         <Contador numClics = {numClics} />
+//         <Boton 
+//           texto='Click'
+//           esBotonDeClic={true}
+//           manejarClic ={manejarClic}
+//         />
+//         <Boton
+//           texto='Reiniciar'
+//           esBotonDeClic={false}
+//           manejarClic ={reiniciarContador}        
+//         />
+//       </div>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import '../css/boton.css';
 
-function Boton({ texto, esBotonDeClic, manejarClic }){
-  return(
-    <button 
-      className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
-      onClick={manejarClic}
-    >
 
-      {texto}
-    </button>
-  );
-
+// Modelo componente de clases
+class Boton extends React.Component{
+  render(){
+    return(
+      <button 
+        className={this.props.esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
+        onClick={this.props.manejarClic}
+      >
+  
+        {this.props.texto}
+      </button>
+    );
+  }
 }
+
+// Modelo componente funcional
+
+// function Boton({ texto, esBotonDeClic, manejarClic }){
+//   return(
+//     <button 
+//       className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
+//       onClick={manejarClic}
+//     >
+
+//       {texto}
+//     </button>
+//   );
+
+// }
 
 export default Boton;
